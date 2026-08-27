614ef50a5152b1e901f13a4d47d297cdb67f61b2
KtGunn <ktgunn@hotmail.com>
2026-08-27 18:44:27 -0400

    emergency module has been created

--- user/user.go
+++ user/user.go
@@ -15,21 +15,29 @@ func main() {
 	signal.Notify(stopSignal, os.Interrupt, syscall.SIGTERM)
 
 	// Create and initialize state module with buffer size of 100
-	sm := NewStateModule(100)
+	sm := NewStateModule(10)
+	em := NewEmergencyModule(13)
 
 	stop := make(chan struct{})
 
 	go func() {
 		log.Println(" Will call LAUNCH SERVER")
-		if err := LaunchServer(50052, stop, sm); err != nil {
+		if err := LaunchServer(50052, stop, sm, em); err != nil {
 			log.Println(" Ooops", err)
 		}
 	}()
 
 	go func() {
 		for {
+			time.Sleep(7 * time.Second)
+			log.Println(" .. Gettin EMG:", em.GetLatestEmergency("user"))
+		}
+	}()
+
+	go func() {
+		for {
 			time.Sleep(5 * time.Second)
-			log.Println(" .. Gettin latests:",sm.GetLatestState("user"))
+			log.Println(" .. Gettin STATUS:", sm.GetLatestState("user"))
 		}
 	}()
 
